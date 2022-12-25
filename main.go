eb6a04e02a44cc41b4fcf5cfef7781abbdaeb858
Thanadon Songsuittipong <thanadon.exe@gmail.com>
2022-12-25 11:06:40 +0700

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
@@ -37,5 +43,32 @@ func main() {
 	protected := r.Group("", auth.Protect([]byte(signature)))
 	handler := todo.NewTodoHandler(db)
 	protected.POST("/todos", handler.NewTask)
-	r.Run()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	s := &http.Server{
+		Handler:        r,
+		Addr:           ":" + os.Getenv("PORT"),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	fmt.Println("gracefully shutting down.. ")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
 }
